68cebce0d2b7effac18931b5e2bec48b1adffccc
Surya Nistala <username@amazon.com>
2020-11-04 22:33:03 +0530

    async search context permits

---
+++ src/main/java/com/amazon/opendistroforelasticsearch/search/async/AsyncSearchContextPermits.java
@@ -0,0 +1,109 @@
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.search.async;
+import com.amazon.opendistroforelasticsearch.search.async.processor.AsyncSearchPostProcessor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ParameterizedMessage;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.common.lease.Releasable;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.util.concurrent.AbstractRunnable;
+import org.elasticsearch.common.util.concurrent.RunOnce;
+import org.elasticsearch.threadpool.ThreadPool;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/***
+ * The permit needed by any mutating operation on {@link AsyncSearchContext} while it is being moved over to the
+ * persistence store. Each mutating operation acquires a single permit while the {@link AsyncSearchPostProcessor} acquires
+ * all permits before it transitions context to the index
+ */
+public class AsyncSearchContextPermits {
+
+    private static final int TOTAL_PERMITS = Integer.MAX_VALUE;
+    final Semaphore semaphore;
+    private final AsyncSearchContextId asyncSearchContextId;
+    private String lockDetails;
+    private final ThreadPool threadPool;
+    private static final Logger logger = LogManager.getLogger(AsyncSearchContextPermits.class);
+
+    public AsyncSearchContextPermits(AsyncSearchContextId asyncSearchContextId, ThreadPool threadPool) {
+        this.asyncSearchContextId = asyncSearchContextId;
+        this.threadPool = threadPool;
+        this.semaphore = new Semaphore(TOTAL_PERMITS, true);
+    }
+
+    private Releasable acquirePermits(int permits, TimeValue timeout, final String details) throws RuntimeException {
+        try {
+            if (semaphore.tryAcquire(permits, timeout.getMillis(), TimeUnit.MILLISECONDS)) {
+                this.lockDetails = details;
+                final RunOnce release = new RunOnce(() -> semaphore.release(permits));
+                return release::run;
+            } else {
+                throw new RuntimeException("obtaining context lock"+ asyncSearchContextId +"timed out after " + timeout.getMillis() + "ms, " +
+                                "previous lock details: [" + lockDetails + "] trying to lock for [" + details + "]");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("thread interrupted while trying to obtain context lock", e);
+        }
+    }
+
+    private void asyncAcquirePermit(int permits, final ActionListener<Releasable> onAcquired, final TimeValue timeout,  String reason)  {
+        threadPool.executor(ThreadPool.Names.GENERIC).execute(new AbstractRunnable() {
+            @Override
+            public void onFailure(final Exception e) {
+                logger.debug(() -> new ParameterizedMessage("Failed to acquire permit {} for {}", permits, reason), e);
+                onAcquired.onFailure(e);
+            }
+
+            @Override
+            protected void doRun()  {
+                final Releasable releasable = acquirePermits(permits, timeout, reason);
+                logger.debug("Successfully acquired permit {} for {}", permits, reason);
+                onAcquired.onResponse(releasable);
+            }
+        });
+    }
+
+    /***
+     * Acquire the permit in an async fashion so as to not block the thread while acquiring. The {@link ActionListener} is invoked if
+     * the mutex was successfully acquired within the timeout. The caller has a responsibility of executing the {@link Releasable}
+     * on completion or failure of the operation run within the permit
+     *
+     * @param onAcquired the releasable that must be invoked
+     * @param timeout the timeout within which the permit must be acquired or deemed failed
+     * @param reason the reason for acquiring the permit
+     */
+    public void asyncAcquirePermit(final ActionListener<Releasable> onAcquired, final TimeValue timeout, String reason)  {
+        asyncAcquirePermit(1, onAcquired, timeout, reason);
+    }
+
+    /***
+     * Acquire all the permits in an async fashion so as to not block the thread while acquiring. The {@link ActionListener} is invoked if
+     * the mutex was successfully acquired within the timeout. The caller has a responsibility of executing the {@link Releasable}
+     * on completion or failure of the operation run within the permit
+     *
+     * @param onAcquired the releasable that must be invoked
+     * @param timeout the timeout within which the permit must be acquired or deemed failed
+     * @param reason the reason for acquiring the permit
+     */
+    public void asyncAcquireAllPermits(final ActionListener<Releasable> onAcquired, final TimeValue timeout, String reason)  {
+        asyncAcquirePermit(TOTAL_PERMITS, onAcquired, timeout, reason);
+    }
+}
