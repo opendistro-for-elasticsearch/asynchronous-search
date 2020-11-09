3c692c122a6e1cb85cecd61a41cfee0019a5fcf2
Surya Nistala <username@amazon.com>
2020-11-09 17:20:02 +0530

    active context

---
+++ src/main/java/com/amazon/opendistroforelasticsearch/search/async/context/active/AsyncSearchActiveContext.java
@@ -0,0 +1,200 @@
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
+package com.amazon.opendistroforelasticsearch.search.async.context.active;
+
+import com.amazon.opendistroforelasticsearch.search.async.AsyncSearchId;
+import com.amazon.opendistroforelasticsearch.search.async.context.AsyncSearchContext;
+import com.amazon.opendistroforelasticsearch.search.async.context.AsyncSearchContextId;
+import com.amazon.opendistroforelasticsearch.search.async.context.permits.AsyncSearchContextPermits;
+import com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage;
+import com.amazon.opendistroforelasticsearch.search.async.listener.AsyncSearchContextListener;
+import com.amazon.opendistroforelasticsearch.search.async.listener.AsyncSearchProgressListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.ParameterizedMessage;
+import org.apache.lucene.util.SetOnce;
+import org.elasticsearch.ResourceNotFoundException;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.search.SearchProgressActionListener;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchTask;
+import org.elasticsearch.common.lease.Releasable;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.threadpool.ThreadPool;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.LongSupplier;
+
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.DELETED;
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.INIT;
+
+/**
+ * The context representing an ongoing search, keeps track of the underlying {@link SearchTask}
+ * and {@link SearchProgressActionListener}
+ */
+public class AsyncSearchActiveContext extends AsyncSearchContext {
+
+    private static final Logger logger = LogManager.getLogger(AsyncSearchActiveContext.class);
+
+    private final SetOnce<SearchTask> searchTask;
+    private volatile long expirationTimeMillis;
+    private volatile long startTimeMillis;
+    private final Boolean keepOnCompletion;
+    private final TimeValue keepAlive;
+    private final String nodeId;
+    private final SetOnce<String> asyncSearchId;
+    private final AtomicBoolean completed;
+    private final SetOnce<Exception> error;
+    private final SetOnce<SearchResponse> searchResponse;
+    private final AsyncSearchContextPermits asyncSearchContextPermits;
+    private volatile AsyncSearchStage asyncSearchStage;
+    private final AsyncSearchContextListener contextListener;
+
+    public AsyncSearchActiveContext(AsyncSearchContextId asyncSearchContextId, String nodeId,
+                                    TimeValue keepAlive, boolean keepOnCompletion,
+                                    ThreadPool threadPool, LongSupplier currentTimeSupplier,
+                                    AsyncSearchProgressListener searchProgressActionListener,
+                                    AsyncSearchContextListener contextListener) {
+        super(asyncSearchContextId, currentTimeSupplier);
+        this.keepOnCompletion = keepOnCompletion;
+        this.error = new SetOnce<>();
+        this.searchResponse = new SetOnce<>();
+        this.keepAlive = keepAlive;
+        this.nodeId = nodeId;
+        this.asyncSearchProgressListener = searchProgressActionListener;
+        this.searchTask = new SetOnce<>();
+        this.asyncSearchId = new SetOnce<>();
+        this.contextListener = contextListener;
+        this.completed = new AtomicBoolean(false);
+        this.asyncSearchContextPermits = new AsyncSearchContextPermits(asyncSearchContextId, threadPool);
+    }
+
+    @Override
+    public AsyncSearchStage getAsyncSearchStage() {
+        assert asyncSearchStage != null : "asyncSearchStage cannot be empty";
+        return asyncSearchStage;
+    }
+
+    @Override
+    public String getAsyncSearchId() {
+        return asyncSearchId.get();
+    }
+
+    public void setTask(SearchTask searchTask) {
+        Objects.requireNonNull(searchTask);
+        searchTask.setProgressListener(asyncSearchProgressListener);
+        this.searchTask.set(searchTask);
+        this.startTimeMillis = searchTask.getStartTime();
+        this.expirationTimeMillis = startTimeMillis + keepAlive.getMillis();
+        this.asyncSearchId.set(AsyncSearchId.buildAsyncId(new AsyncSearchId(nodeId, searchTask.getId(), getContextId())));
+        advanceStage(INIT);
+    }
+
+    public SearchTask getTask() {
+        return searchTask.get();
+    }
+
+    public boolean shouldPersist() {
+        return keepOnCompletion && isExpired() == false && asyncSearchStage != DELETED;
+    }
+
+    public void processSearchFailure(Exception e) {
+        if (completed.compareAndSet(false, true)) {
+            error.set(e);
+            advanceStage(AsyncSearchStage.FAILED);
+        } else {
+            throw new IllegalStateException("Cannot process search failure event for ["
+                    + asyncSearchContextId + "] . Search has already completed.");
+        }
+    }
+
+    public void processSearchResponse(SearchResponse response) {
+
+        if (completed.compareAndSet(false, true)) {
+            this.searchResponse.set(response);
+            advanceStage(AsyncSearchStage.SUCCEEDED);
+        } else {
+            throw new IllegalStateException("Cannot process search response event for ["
+                    + asyncSearchContextId + "] . Search has already completed.");
+        }
+    }
+
+    @Override
+    public SearchResponse getSearchResponse() {
+        if (completed.get()) {
+            return searchResponse.get();
+        } else {
+            return asyncSearchProgressListener.partialResponse();
+        }
+    }
+
+    @Override
+    public Exception getSearchError() {
+        return error.get();
+    }
+
+    @Override
+    public long getExpirationTimeMillis() {
+        return expirationTimeMillis;
+    }
+
+    @Override
+    public long getStartTimeMillis() {
+        return startTimeMillis;
+    }
+
+
+    /**
+     * Atomically validates and updates the state of the search
+     *
+     * @param nextAsyncSearchStage asyncSearchStage to advance
+     */
+    public synchronized void advanceStage(AsyncSearchStage nextAsyncSearchStage) {
+        AsyncSearchStage currentAsyncSearchStage = asyncSearchStage;
+
+        if (currentAsyncSearchStage == null && nextAsyncSearchStage != INIT) {
+            throw new IllegalStateException("only next asyncSearchStage " + INIT +
+                    " is allowed when the current asyncSearchStage is null");
+        } else if (currentAsyncSearchStage != null &&
+                asyncSearchStage.nextTransitions().contains(nextAsyncSearchStage) == false) {
+            // Handle concurrent deletes race condition
+            if (currentAsyncSearchStage == DELETED && nextAsyncSearchStage == DELETED) {
+                throw new ResourceNotFoundException(getAsyncSearchId());
+            }
+            throw new IllegalStateException(
+                    "can't move to asyncSearchStage [" + nextAsyncSearchStage + "], from current asyncSearchStage: ["
+                            + currentAsyncSearchStage + "] (valid states [" + currentAsyncSearchStage.nextTransitions() + "])");
+        }
+        asyncSearchStage = nextAsyncSearchStage;
+        if (currentAsyncSearchStage != nextAsyncSearchStage) {
+            try {
+                asyncSearchStage.onTransition(contextListener, getContextId());
+            } catch (Exception ex) {
+                logger.error(() -> new ParameterizedMessage("Failed to execute context listener for transition " +
+                        "from stage {} to stage {}", currentAsyncSearchStage, nextAsyncSearchStage), ex);
+            }
+        }
+    }
+
+    public void acquireContextPermit(final ActionListener<Releasable> onPermitAcquired, TimeValue timeout, String reason) {
+        asyncSearchContextPermits.asyncAcquirePermit(onPermitAcquired, timeout, reason);
+    }
+
+    public void acquireAllContextPermits(final ActionListener<Releasable> onPermitAcquired, TimeValue timeout, String reason) {
+        asyncSearchContextPermits.asyncAcquireAllPermits(onPermitAcquired, timeout, reason);
+    }
+}
---
+++ src/test/java/com/amazon/opendistroforelasticsearch/search/async/context/active/AsyncSearchActiveContextTests.java
@@ -0,0 +1,299 @@
+package com.amazon.opendistroforelasticsearch.search.async.context.active;
+
+import com.amazon.opendistroforelasticsearch.search.async.AsyncSearchId;
+import com.amazon.opendistroforelasticsearch.search.async.context.AsyncSearchContextId;
+import com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage;
+import com.amazon.opendistroforelasticsearch.search.async.listener.AsyncSearchContextListener;
+import com.amazon.opendistroforelasticsearch.search.async.listener.AsyncSearchProgressListener;
+import com.amazon.opendistroforelasticsearch.search.async.response.AsyncSearchResponse;
+import org.apache.lucene.search.TotalHits;
+import org.elasticsearch.ResourceNotFoundException;
+import org.elasticsearch.action.search.SearchAction;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchTask;
+import org.elasticsearch.action.search.ShardSearchFailure;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.InternalAggregations;
+import org.elasticsearch.search.internal.InternalSearchResponse;
+import org.elasticsearch.search.profile.SearchProfileShardResults;
+import org.elasticsearch.search.suggest.Suggest;
+import org.elasticsearch.test.ESTestCase;
+import org.elasticsearch.threadpool.TestThreadPool;
+import org.elasticsearch.threadpool.ThreadPool;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.DELETED;
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.FAILED;
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.INIT;
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.PERSISTED;
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.PERSIST_FAILED;
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.RUNNING;
+import static com.amazon.opendistroforelasticsearch.search.async.context.stage.AsyncSearchStage.SUCCEEDED;
+
+public class AsyncSearchActiveContextTests extends ESTestCase {
+
+    private final String node = UUID.randomUUID().toString();
+
+    public void testAsyncSearchStageTransitionsForAsyncSearchLifeCycle() {
+        TestThreadPool threadPool = null;
+        try {
+            threadPool = new TestThreadPool("test");
+            AsyncSearchProgressListener asyncSearchProgressListener = new AsyncSearchProgressListener(
+                    threadPool.absoluteTimeInMillis(), r -> null, e -> null, threadPool.generic(), threadPool::relativeTimeInMillis);
+            AsyncSearchContextId asyncSearchContextId = new AsyncSearchContextId(UUID.randomUUID().toString(),
+                    randomNonNegativeLong());
+            boolean keepOnCompletion = randomBoolean();
+            TimeValue keepAlive = TimeValue.timeValueDays(randomInt(100));
+            AsyncSearchActiveContext context = new AsyncSearchActiveContext(asyncSearchContextId, node,
+                    keepAlive, keepOnCompletion, threadPool,
+                    threadPool::absoluteTimeInMillis, asyncSearchProgressListener, new AsyncSearchContextListener() {
+            });
+
+            //Before we register search task
+            verifyIllegalStageAdvancementFailure(context, Arrays.asList(RUNNING, FAILED, DELETED, PERSIST_FAILED,
+                    PERSISTED, SUCCEEDED));
+
+            //After task is registered, before search result is received
+            assertNull(context.getTask());
+            SearchTask task = new SearchTask(randomNonNegativeLong(), "transport", SearchAction.NAME,
+                    null, null, Collections.emptyMap());
+            context.setTask(task);
+            verifyInitStageInfo(context, task, keepAlive);
+
+            //Set to RUNNING once search task has begun running
+            context.advanceStage(RUNNING);
+            assertEquals(RUNNING, context.getAsyncSearchStage());
+            verifyIllegalStageAdvancementFailure(context, Arrays.asList(RUNNING, null, INIT));
+
+
+            boolean success = randomBoolean();
+            if (success) {
+                //receiving a response from search on completion
+                SearchResponse response = getMockSearchResponse();
+                context.processSearchResponse(response);
+                verifySucceededStageInfo(context, response, new AsyncSearchResponse(AsyncSearchId.buildAsyncId(new AsyncSearchId(node,
+                        task.getId(), context.getContextId())), false, task.getStartTime(),
+                        task.getStartTime() + keepAlive.millis(), response, null));
+            } else {
+                //receiving an exception from search
+                Exception exception = new RuntimeException("test");
+                context.processSearchFailure(exception);
+                verifyFailedStageInfo(context, exception, new AsyncSearchResponse(AsyncSearchId.buildAsyncId(new AsyncSearchId(node,
+                        task.getId(), context.getContextId())), false, task.getStartTime(),
+                        task.getStartTime() + keepAlive.millis(), null, exception));
+            }
+
+            // persisting a complete search or failure to do so
+            boolean persisted = randomBoolean();
+            if (persisted) {
+                context.advanceStage(PERSISTED);
+                verifyIllegalStageAdvancementFailure(context, Arrays.asList(null, INIT, RUNNING, SUCCEEDED, PERSISTED,
+                        PERSIST_FAILED));
+            } else {
+                context.advanceStage(PERSIST_FAILED);
+                verifyIllegalStageAdvancementFailure(context, Arrays.asList(null, INIT, RUNNING, SUCCEEDED, PERSISTED,
+                        PERSIST_FAILED));
+            }
+
+            context.advanceStage(DELETED);
+            assertEquals(context.getAsyncSearchStage(), DELETED);
+        } finally {
+            ThreadPool.terminate(threadPool, 30, TimeUnit.SECONDS);
+        }
+    }
+
+    public void testDeletionOfRunningAsyncSearch() {
+        TestThreadPool threadPool = null;
+        try {
+            threadPool = new TestThreadPool("test");
+            AsyncSearchProgressListener asyncSearchProgressListener = new AsyncSearchProgressListener(
+                    threadPool.absoluteTimeInMillis(), r -> null, e -> null, threadPool.generic(), threadPool::relativeTimeInMillis);
+            AsyncSearchContextId asyncSearchContextId = new AsyncSearchContextId(UUID.randomUUID().toString(),
+                    randomNonNegativeLong());
+            boolean keepOnCompletion = true;
+            TimeValue keepAlive = TimeValue.timeValueDays(randomInt(100));
+            AsyncSearchActiveContext context = new AsyncSearchActiveContext(asyncSearchContextId, node,
+                    keepAlive, keepOnCompletion, threadPool,
+                    threadPool::absoluteTimeInMillis, asyncSearchProgressListener, new AsyncSearchContextListener() {
+            });
+            context.setTask(new SearchTask(randomNonNegativeLong(), "transport", SearchAction.NAME,
+                    null, null, Collections.emptyMap()));
+            context.advanceStage(RUNNING);
+            assertTrue(context.shouldPersist());
+            context.advanceStage(DELETED);
+            assertFalse(context.shouldPersist());
+            assertEquals(context.getAsyncSearchStage(), DELETED);
+        } finally {
+            ThreadPool.terminate(threadPool, 30, TimeUnit.SECONDS);
+        }
+    }
+
+    public void testAsyncSearchContextProcessesCompletionOnlyOnce() throws InterruptedException {
+        TestThreadPool threadPool = null;
+        try {
+            threadPool = new TestThreadPool("test");
+            AsyncSearchProgressListener asyncSearchProgressListener = new AsyncSearchProgressListener(
+                    threadPool.absoluteTimeInMillis(), r -> null, e -> null, threadPool.generic(), threadPool::relativeTimeInMillis);
+            AsyncSearchContextId asyncSearchContextId = new AsyncSearchContextId(UUID.randomUUID().toString(),
+                    randomNonNegativeLong());
+            boolean keepOnCompletion = randomBoolean();
+            TimeValue keepAlive = TimeValue.timeValueDays(randomInt(100));
+            AsyncSearchActiveContext context = new AsyncSearchActiveContext(asyncSearchContextId, node,
+                    keepAlive, keepOnCompletion, threadPool,
+                    threadPool::absoluteTimeInMillis, asyncSearchProgressListener, new AsyncSearchContextListener() {
+            });
+            context.setTask(new SearchTask(randomNonNegativeLong(), "transport", SearchAction.NAME,
+                    null, null, Collections.emptyMap()));
+            context.advanceStage(RUNNING);
+
+            int numThread = 10;
+            AtomicInteger completions = new AtomicInteger(0);
+            AtomicInteger processingFailures = new AtomicInteger(0);
+            List<Thread> operationThreads = new ArrayList<>();
+
+            for (int i = 0; i < numThread; i++) {
+                //randomly choose to process search or failure
+                Runnable runnable = randomBoolean() ? () -> {
+                    try {
+                        context.processSearchResponse(getMockSearchResponse());
+                        completions.getAndIncrement();
+                    } catch (IllegalStateException e) {
+                        processingFailures.getAndIncrement();
+                    }
+                } : () -> {
+                    try {
+                        context.processSearchFailure(new RuntimeException("test"));
+                        completions.getAndIncrement();
+                    } catch (IllegalStateException e) {
+                        processingFailures.getAndIncrement();
+                    }
+                };
+                operationThreads.add(new Thread(runnable));
+            }
+            operationThreads.forEach(Thread::start);
+            for (Thread thread : operationThreads) {
+                //not using forEach as method throws Interrupted Exception
+                thread.join();
+            }
+            assertEquals(completions.get(), 1);
+            assertEquals(processingFailures.get(), 9);
+            assertTrue(context.getAsyncSearchStage() == SUCCEEDED || context.getAsyncSearchStage() == FAILED);
+        } finally {
+            ThreadPool.terminate(threadPool, 30, TimeUnit.SECONDS);
+        }
+    }
+
+    public void testConcurrentUpdatesToAsyncSearchStage() throws InterruptedException {
+        TestThreadPool threadPool = null;
+        try {
+            threadPool = new TestThreadPool("test");
+            AsyncSearchProgressListener asyncSearchProgressListener = new AsyncSearchProgressListener(
+                    threadPool.absoluteTimeInMillis(), r -> null, e -> null, threadPool.generic(), threadPool::relativeTimeInMillis);
+            AsyncSearchContextId asyncSearchContextId = new AsyncSearchContextId(UUID.randomUUID().toString(),
+                    randomNonNegativeLong());
+            boolean keepOnCompletion = randomBoolean();
+            TimeValue keepAlive = TimeValue.timeValueDays(randomInt(100));
+            AsyncSearchActiveContext context = new AsyncSearchActiveContext(asyncSearchContextId, node,
+                    keepAlive, keepOnCompletion, threadPool,
+                    threadPool::absoluteTimeInMillis, asyncSearchProgressListener, new AsyncSearchContextListener() {
+            });
+            doConcurrentStageAdvancement(context, INIT, IllegalStateException.class);
+            doConcurrentStageAdvancement(context, RUNNING, IllegalStateException.class);
+            if (randomBoolean()) {//success
+                doConcurrentStageAdvancement(context, SUCCEEDED, IllegalStateException.class);
+            } else {
+                doConcurrentStageAdvancement(context, FAILED, IllegalStateException.class);
+            }
+
+            if (randomBoolean()) { //persistence succeeded
+                doConcurrentStageAdvancement(context, PERSISTED, IllegalStateException.class);
+            } else {
+                doConcurrentStageAdvancement(context, PERSIST_FAILED, IllegalStateException.class);
+            }
+
+            doConcurrentStageAdvancement(context, DELETED, ResourceNotFoundException.class);
+        } finally {
+            ThreadPool.terminate(threadPool, 30, TimeUnit.SECONDS);
+        }
+    }
+
+    private <T extends Throwable> void doConcurrentStageAdvancement(AsyncSearchActiveContext context,
+                                                                    AsyncSearchStage stage,
+                                                                    Class<T> throwable) throws InterruptedException {
+        int numThreads = 10;
+
+        List<Thread> operationThreads = new ArrayList<>();
+        AtomicInteger numUpdateSuccesses = new AtomicInteger();
+        for (int i = 0; i < numThreads; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    context.advanceStage(stage);
+                    numUpdateSuccesses.getAndIncrement();
+                } catch (Exception e) {
+                    assertTrue(throwable.isInstance(e));
+                }
+            });
+            operationThreads.add(thread);
+        }
+        operationThreads.forEach(Thread::start);
+        for (Thread operationThread : operationThreads) {
+            operationThread.join();
+        }
+        assertEquals(numUpdateSuccesses.get(), 1);
+        assertEquals(context.getAsyncSearchStage(), stage);
+    }
+
+    private void verifyFailedStageInfo(AsyncSearchActiveContext context, Exception exception,
+                                       AsyncSearchResponse asyncSearchResponse) {
+        assertNull(context.getSearchResponse());
+        assertEquals(context.getSearchError(), exception);
+        assertEquals(context.getAsyncSearchResponse(), asyncSearchResponse);
+        verifyIllegalStageAdvancementFailure(context, Arrays.asList(null, INIT, SUCCEEDED, FAILED));
+    }
+
+    private void verifySucceededStageInfo(AsyncSearchActiveContext context, SearchResponse response,
+                                          AsyncSearchResponse asyncSearchResponse) {
+        assertNull(context.getSearchError());
+        assertEquals(context.getSearchResponse(), response);
+        assertEquals(context.getAsyncSearchResponse(), asyncSearchResponse);
+        verifyIllegalStageAdvancementFailure(context, Arrays.asList(null, INIT, SUCCEEDED, FAILED));
+
+    }
+
+    private void verifyInitStageInfo(AsyncSearchActiveContext asyncSearchActiveContext, SearchTask task, TimeValue keepAlive) {
+        assertEquals(INIT, asyncSearchActiveContext.getAsyncSearchStage());
+        assertEquals(asyncSearchActiveContext.getTask(), task);
+        assertEquals(asyncSearchActiveContext.getStartTimeMillis(), task.getStartTime());
+        assertEquals(task.getStartTime() + keepAlive.millis(), asyncSearchActiveContext.getExpirationTimeMillis());
+        assertEquals(asyncSearchActiveContext.getAsyncSearchId(), AsyncSearchId.buildAsyncId(new AsyncSearchId(node,
+                task.getId(), asyncSearchActiveContext.getContextId())));
+        verifyIllegalStageAdvancementFailure(asyncSearchActiveContext, Arrays.asList(INIT, null, PERSIST_FAILED, DELETED,
+                PERSISTED, FAILED, SUCCEEDED));
+    }
+
+    private void verifyIllegalStageAdvancementFailure(AsyncSearchActiveContext asyncSearchActiveContext,
+                                                      Collection<AsyncSearchStage> illegalNextStageList) {
+        illegalNextStageList.forEach(stage -> expectThrows(IllegalStateException.class,
+                () -> asyncSearchActiveContext.advanceStage(stage)));
+    }
+
+    private SearchResponse getMockSearchResponse() {
+        return new SearchResponse(new InternalSearchResponse(
+                new SearchHits(new SearchHit[0], new TotalHits(0L, TotalHits.Relation.EQUAL_TO), 0.0f),
+                new InternalAggregations(Collections.emptyList()),
+                new Suggest(Collections.emptyList()),
+                new SearchProfileShardResults(Collections.emptyMap()), false, false, 1),
+                "", 1, 1, 0, 0,
+                ShardSearchFailure.EMPTY_ARRAY, SearchResponse.Clusters.EMPTY);
+    }
+}
