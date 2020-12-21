99aacde2c5a3f467f36c35a1b7f4b2a09d45f4eb
Bukhtawar Khan <bukhtawa@amazon.com>
2020-12-21 19:42:41 +0530

    Management layer changes for PR feedback

--- src/main/java/com/amazon/opendistroforelasticsearch/search/async/management/AsyncSearchManagementService.java
+++ src/main/java/com/amazon/opendistroforelasticsearch/search/async/management/AsyncSearchManagementService.java
@@ -20,13 +20,16 @@ import com.amazon.opendistroforelasticsearch.search.async.context.persistence.As
 import com.amazon.opendistroforelasticsearch.search.async.plugin.AsyncSearchPlugin;
 import com.amazon.opendistroforelasticsearch.search.async.response.AcknowledgedResponse;
 import com.amazon.opendistroforelasticsearch.search.async.service.AsyncSearchService;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestValidationException;
-import org.elasticsearch.cluster.LocalNodeMasterListener;
+import org.elasticsearch.cluster.ClusterChangedEvent;
+import org.elasticsearch.cluster.ClusterState;
+import org.elasticsearch.cluster.ClusterStateListener;
 import org.elasticsearch.cluster.node.DiscoveryNode;
 import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.Booleans;
@@ -36,10 +39,11 @@ import org.elasticsearch.common.component.AbstractLifecycleComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.io.stream.StreamInput;
 import org.elasticsearch.common.io.stream.StreamOutput;
-import org.elasticsearch.common.lease.Releasable;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.util.concurrent.AbstractRunnable;
+import org.elasticsearch.common.util.concurrent.EsRejectedExecutionException;
 import org.elasticsearch.common.util.concurrent.ThreadContext;
 import org.elasticsearch.tasks.Task;
 import org.elasticsearch.threadpool.Scheduler;
@@ -54,7 +58,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -63,7 +66,7 @@ import java.util.stream.Stream;
  * The service takes care of cancelling ongoing searches which have been running past their expiration time and cleaning up async search
  * responses from disk by scheduling delete-by-query on master to be delegated to the least loaded node
  */
-public class AsyncSearchManagementService extends AbstractLifecycleComponent implements LocalNodeMasterListener {
+public class AsyncSearchManagementService extends AbstractLifecycleComponent implements ClusterStateListener {
 
     private static final Logger logger = LogManager.getLogger(AsyncSearchManagementService.class);
 
@@ -71,7 +74,8 @@ public class AsyncSearchManagementService extends AbstractLifecycleComponent imp
     private final AsyncSearchPersistenceService asyncSearchPersistenceService;
     private final ThreadPool threadPool;
     private volatile Scheduler.Cancellable taskReaperScheduledFuture;
-    private AtomicReference<ResponseCleanUpScheduler> activeResponseCleanUpScheduler = new AtomicReference<>();
+    private static final String RESPONSE_CLEANUP_SCHEDULING_EXECUTOR = ThreadPool.Names.MANAGEMENT;
+    private AtomicReference<ResponseCleanUpAndRescheduleRunnable> activeResponseCleanUpRunnable = new AtomicReference<>();
     private AsyncSearchService asyncSearchService;
     private TransportService transportService;
     private TimeValue taskCancellationInterval;
@@ -94,7 +98,7 @@ public class AsyncSearchManagementService extends AbstractLifecycleComponent imp
                                         AsyncSearchPersistenceService asyncSearchPersistenceService) {
         this.clusterService = clusterService;
         this.threadPool = threadPool;
-        this.clusterService.addLocalNodeMasterListener(this);
+        this.clusterService.addListener(this);
         this.asyncSearchService = asyncSearchService;
         this.transportService = transportService;
         this.asyncSearchPersistenceService = asyncSearchPersistenceService;
@@ -131,28 +135,25 @@ public class AsyncSearchManagementService extends AbstractLifecycleComponent imp
     }
 
     @Override
-    public void onMaster() {
-        ResponseCleanUpScheduler cleanupScheduler = new ResponseCleanUpScheduler();
-        ResponseCleanUpScheduler previousScheduler = activeResponseCleanUpScheduler.getAndSet(cleanupScheduler);
-        if (previousScheduler != null) {
-            previousScheduler.close();
-        }
-        if (cleanupScheduler != null) {
-            cleanupScheduler.handleWakeUp();
+    public void clusterChanged(ClusterChangedEvent event) {
+        if (event.localNodeMaster() && activeResponseCleanUpRunnable.get() == null) {
+            logger.trace("elected as master, scheduling cluster info update tasks");
+            executeRefresh(event.state(), "became master");
+
+            final ResponseCleanUpAndRescheduleRunnable newRunnable = new ResponseCleanUpAndRescheduleRunnable();
+            activeResponseCleanUpRunnable.set(newRunnable);
+            threadPool.scheduleUnlessShuttingDown(responseCleanUpInterval, RESPONSE_CLEANUP_SCHEDULING_EXECUTOR, newRunnable);
+        } else if (event.localNodeMaster() == false) {
+            activeResponseCleanUpRunnable.set(null);
+            return;
         }
     }
 
-    @Override
-    public void offMaster() {
-        ResponseCleanUpScheduler cleanUpScheduler = activeResponseCleanUpScheduler.get();
-        if (cleanUpScheduler != null) {
-            cleanUpScheduler.close();
+    private void executeRefresh(ClusterState clusterState, String reason) {
+        if (clusterState.nodes().getDataNodes().size() > 1) {
+            logger.trace("refreshing cluster info in background [{}]", reason);
+            threadPool.executor(RESPONSE_CLEANUP_SCHEDULING_EXECUTOR).execute(new ResponseCleanUpRunnable(reason));
         }
-    }
-
-    @Override
-    public String executorName() {
-        return AsyncSearchPlugin.OPEN_DISTRO_ASYNC_SEARCH_GENERIC_THREAD_POOL_NAME;
     }
 
     @Override
@@ -163,19 +164,13 @@ public class AsyncSearchManagementService extends AbstractLifecycleComponent imp
 
     @Override
     protected void doStop() {
-        ResponseCleanUpScheduler cleanUpScheduler = activeResponseCleanUpScheduler.get();
-        if (cleanUpScheduler != null) {
-            cleanUpScheduler.close();
-        }
+        activeResponseCleanUpRunnable.set(null);
         taskReaperScheduledFuture.cancel();
     }
 
     @Override
     protected void doClose() {
-        ResponseCleanUpScheduler cleanUpScheduler = activeResponseCleanUpScheduler.get();
-        if (cleanUpScheduler != null) {
-            cleanUpScheduler.close();
-        }
+        activeResponseCleanUpRunnable.set(null);
         taskReaperScheduledFuture.cancel();
     }
 
@@ -203,99 +198,108 @@ public class AsyncSearchManagementService extends AbstractLifecycleComponent imp
         }
     }
 
-    class ResponseCleanUpScheduler implements Releasable {
-
-        private final AtomicBoolean isClosed = new AtomicBoolean();
-
-        void handleWakeUp() {
-            if (isClosed.get()) {
-                logger.trace("closed check scheduler woken up, doing nothing");
+    public final void performCleanUp() {
+        final ThreadContext threadContext = threadPool.getThreadContext();
+        try (ThreadContext.StoredContext ignore = threadContext.stashContext()) {
+            // we have to execute under the system context so that if security is enabled the sync is authorized
+            threadContext.markAsSystemContext();
+            ImmutableOpenMap<String, DiscoveryNode> dataNodes = clusterService.state().nodes().getDataNodes();
+            List<DiscoveryNode> nodes = Stream.of(dataNodes.values().toArray(DiscoveryNode.class))
+                    .filter((node) -> isAsyncSearchEnabledNode(node)).collect(Collectors.toList());
+            if (nodes == null || nodes.isEmpty()) {
+                logger.debug("Found empty data nodes with async search enabled attribute [{}] for response clean up," +
+                        " scheduling next wake up!", dataNodes);
                 return;
             }
+            int pos = Randomness.get().nextInt(nodes.size());
+            DiscoveryNode randomNode = nodes.get(pos);
+            transportService.sendRequest(randomNode, CLEANUP_ACTION_NAME,
+                    new AsyncSearchCleanUpRequest(threadPool.absoluteTimeInMillis()),
+                    new TransportResponseHandler<AcknowledgedResponse>() {
 
-            final ThreadContext threadContext = threadPool.getThreadContext();
-            try (ThreadContext.StoredContext ignore = threadContext.stashContext()) {
-                // we have to execute under the system context so that if security is enabled the sync is authorized
-                threadContext.markAsSystemContext();
-                ImmutableOpenMap<String, DiscoveryNode> dataNodes = clusterService.state().nodes().getDataNodes();
-                List<DiscoveryNode> nodes = Stream.of(dataNodes.values().toArray(DiscoveryNode.class))
-                        .filter((node) -> isAsyncSearchEnabledNode(node)).collect(Collectors.toList());
-                if (nodes == null || nodes.isEmpty()) {
-                    logger.debug("Found empty data nodes with async search enabled attribute [{}] for response clean up," +
-                            " scheduling next wake up!", dataNodes);
-                    scheduleNextWakeUp();
-                    return;
-                }
-                int pos = Randomness.get().nextInt(nodes.size());
-                DiscoveryNode randomNode = nodes.get(pos);
-                transportService.sendRequest(randomNode, CLEANUP_ACTION_NAME,
-                        new AsyncSearchCleanUpRequest(threadPool.absoluteTimeInMillis()),
-                        new TransportResponseHandler<AcknowledgedResponse>() {
+                        @Override
+                        public AcknowledgedResponse read(StreamInput in) throws IOException {
+                            return new AcknowledgedResponse(in);
+                        }
 
-                            @Override
-                            public AcknowledgedResponse read(StreamInput in) throws IOException {
-                                return new AcknowledgedResponse(in);
-                            }
+                        @Override
+                        public void handleResponse(AcknowledgedResponse response) {
+                            logger.debug("Successfully executed clean up action on node {} with response {}", randomNode,
+                                    response.isAcknowledged());
+                        }
 
-                            @Override
-                            public void handleResponse(AcknowledgedResponse response) {
-                                if (isClosed.get()) {
-                                    logger.debug("closed check scheduler received a response, doing nothing");
-                                    return;
-                                }
-                                logger.debug("Successfully executed clean up action on node {} with response {}", randomNode,
-                                        response.isAcknowledged());
-                                scheduleNextWakeUp(); // logs trace message indicating success
-                            }
+                        @Override
+                        public void handleException(TransportException e) {
+                            logger.error(() -> new ParameterizedMessage("Exception executing action {}",
+                                    CLEANUP_ACTION_NAME), e);
+                        }
 
-                            @Override
-                            public void handleException(TransportException e) {
-                                logger.error(() -> new ParameterizedMessage("Exception executing action {}",
-                                        CLEANUP_ACTION_NAME), e);
-                                scheduleNextWakeUp();
-                            }
+                        @Override
+                        public String executor() {
+                            return AsyncSearchPlugin.OPEN_DISTRO_ASYNC_SEARCH_GENERIC_THREAD_POOL_NAME;
+                        }
+                    });
 
-                            @Override
-                            public String executor() {
-                                return AsyncSearchPlugin.OPEN_DISTRO_ASYNC_SEARCH_GENERIC_THREAD_POOL_NAME;
-                            }
-                        });
-
-            } catch (Exception ex) {
-                logger.error("Failed to schedule async search cleanup", ex);
-                scheduleNextWakeUp();
-            }
+        } catch (Exception ex) {
+            logger.error("Failed to schedule async search cleanup", ex);
         }
+    }
 
-        // TODO: only here temporarily for BWC development, remove once complete
-        private boolean isAsyncSearchEnabledNode(DiscoveryNode discoveryNode) {
-            return Booleans.isTrue(discoveryNode.getAttributes().getOrDefault("asynchronous_search_enabled", "false"));
-        }
+    // TODO: only here temporarily for BWC development, remove once complete
+    private boolean isAsyncSearchEnabledNode(DiscoveryNode discoveryNode) {
+        return Booleans.isTrue(discoveryNode.getAttributes().getOrDefault("asynchronous_search_enabled", "false"));
+    }
 
-        private void scheduleNextWakeUp() {
-            logger.trace("scheduling next check for [{}] = {}", RESPONSE_CLEAN_UP_INTERVAL_SETTING.getKey(), responseCleanUpInterval);
-            transportService.getThreadPool().schedule(new Runnable() {
-                @Override
-                public void run() {
-                    handleWakeUp();
-                }
 
-                @Override
-                public String toString() {
-                    return "scheduled check of clean up action ";
-                }
-            }, responseCleanUpInterval, ThreadPool.Names.SAME);
+    private class ResponseCleanUpRunnable extends AbstractRunnable {
+        private final String reason;
+
+        ResponseCleanUpRunnable(String reason) {
+            this.reason = reason;
         }
 
         @Override
-        public void close() {
-            if (isClosed.compareAndSet(false, true) == false) {
-                logger.trace("already closed, doing nothing");
+        protected void doRun() {
+            performCleanUp();
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            logger.warn(new ParameterizedMessage("sync search clean up job failed [{}]", reason), e);
+        }
+
+
+        @Override
+        public void onRejection(Exception e) {
+            final boolean shutDown = e instanceof EsRejectedExecutionException && ((EsRejectedExecutionException) e).isExecutorShutdown();
+            logger.log(shutDown ? Level.DEBUG : Level.WARN, "async search clean up job rejected [{}]", reason, e);
+        }
+    }
+
+
+    private class ResponseCleanUpAndRescheduleRunnable extends ResponseCleanUpRunnable {
+        ResponseCleanUpAndRescheduleRunnable() {
+            super("scheduled");
+        }
+
+        @Override
+        protected void doRun() {
+            if (this == activeResponseCleanUpRunnable.get()) {
+                super.doRun();
             } else {
-                logger.debug("closed");
+                logger.trace("master changed, scheduled cleanup job is stale");
+            }
+        }
+
+        @Override
+        public void onAfter() {
+            if (this == activeResponseCleanUpRunnable.get()) {
+                logger.trace("scheduling next clean up job in [{}]", responseCleanUpInterval);
+                threadPool.scheduleUnlessShuttingDown(responseCleanUpInterval, RESPONSE_CLEANUP_SCHEDULING_EXECUTOR, this);
             }
         }
     }
+
 
     static class AsyncSearchCleanUpRequest extends ActionRequest {
 
